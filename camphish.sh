afdaad033153cf19114ddf70f8b1b7aa7b7b73ef
Tech Hacker <114179175+TechHacker737@users.noreply.github.com>
2023-04-29 21:05:50 +0530

    grapsnap.sh

--- camphish.sh
+++ camphish.sh
@@ -15,7 +15,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
 printf " \e[1;93m GrapSnap Ver 1.0 \e[0m \n"
-printf " \e[1;77m youtube.com/techhacker7 \e[0m \n"
+printf " \e[1;77m www.youtube.com/techhacker7 \e[0m \n"
 
 printf "\n"
 
